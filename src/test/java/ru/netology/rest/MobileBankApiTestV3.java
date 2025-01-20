eb44de73404657686a02b781489417dc46d58b7b
KseniaYudina <uksenia359@gmail.com>
2025-01-21 00:32:00 +0300

    modified build.gradle testv3

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -4,6 +4,7 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV3 {
@@ -13,16 +14,17 @@ class MobileBankApiTestV3 {
         // Предусловия
         given()
                 .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
+
                 .when()
                 .get("/demo/accounts")
-                // Проверки
+
                 .then()
                 .statusCode(200)
-                // специализированные проверки - лучше
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+
                 .contentType(ContentType.JSON)
                 .body("", hasSize(3))
-                .body("[0].currency", equalTo("RUB"))
+                .body("[0].currency", equalTo("USD"))
                 .body("[0].balance", greaterThanOrEqualTo(0))
         ;
     }
